44b57187b839e4ad071b21dbef80463e87e1f106
Boonyakorn Wasusopon <trust_wasu@hotmail.com>
2017-05-05 15:13:35 -0400

    add basic structure to website

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'pages#about'
-  get 'pages/about'
+  resources :listings
+  get "pages/about"
+  get "pages/contact"
 
-  get 'pages/contact'
+  root 'listings#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
